dba78bf164f30c46231d79c801fd9f29a3766d4a
Web-Coder2023 <denis.tihonkih.frontend.dumix@yandex.ru>
2024-12-07 17:37:28 +0300

    ...

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import https from "https";
+import fs from "fs";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -11,36 +13,52 @@ const app = express();
 
 // Middleware
 const allowedOrigins = [
-    'http://localhost:3000',
-    'http://45.67.58.204:3000'
-  ];
-  
-  app.use(cors({
+  'https://webcraft-app.ru', // всегда разрешаем для продакшн
+];
+
+// Добавляем разрешение для локальной разработки, если NODE_ENV = development
+if (process.env.NODE_ENV === "development") {
+  allowedOrigins.push('http://localhost:3000'); // добавляем для локальной разработки
+}
+
+app.use(cors({
     origin: function (origin, callback) {
-      // Разрешаем запросы с указанных источников, если origin присутствует в allowedOrigins
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true); // null - отсутствие ошибок, true - разрешение запроса
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true); // null - отсутствие ошибок, true - разрешение запроса
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
     }
-  }));
+}));
+
 app.use(express.json());
 
 // Подключение к MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log(err));
-
-// Маршруты
 app.use("/api/auth", authRoutes);
-app.use("/api", eventRoutes); // Подключаем маршрут для событий
+app.use("/api", eventRoutes); 
 
-// Запуск сервера
-const port = process.env.PORT || 8000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// Для продакшн окружения
+let server;
+if (process.env.NODE_ENV === "production") {
+    // При деплое используем HTTPS с сертификатами
+    const options = {
+        key: fs.readFileSync('/etc/letsencrypt/live/webcraft-app.ru/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/webcraft-app.ru/fullchain.pem'),
+        ca: fs.readFileSync('/etc/letsencrypt/live/webcraft-app.ru/chain.pem')
+    };
+
+    server = https.createServer(options, app);
+    const port = process.env.PORT || 8000;
+    server.listen(port, () => {
+        console.log(`Server is running on https://webcraft-app.ru:${port}`);
+    });
+} else {
+    // Для разработки используем HTTP
+    const port = process.env.PORT || 8000;
+    server = app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+}
